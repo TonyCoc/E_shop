70efbfdc8704ac618c391d374ae13c732a788205
Kian <78940146+WinchesterAge@users.noreply.github.com>
2022-07-05 00:08:47 +0430

    admin panel users list (commit 3)

--- eshop_admin/urls.py
+++ eshop_admin/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include,path
 from .views import *
 urlpatterns = [
-    path('admin-panel',admin_home)
+    path('admin-panel',admin_home),
+    path('admin-panel/users-list',user_list.as_view())
 ]
--- eshop_admin/views.py
+++ eshop_admin/views.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
-
+from django.views.generic import ListView
 
 
 def admin_home(request):
     return render(request, "admin_panel/Home/admin_home.html")
+
+class user_list(ListView):
+    template_name = "admin_panel/Users/User_lists.html"
+    model = User
+
 
 def _sidebar_partial(request):
     return render(request, "admin_panel/partials/_sidebar.html")
