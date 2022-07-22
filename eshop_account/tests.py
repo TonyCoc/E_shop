20d1e44266ae9f4d9f3283af37c46f9e677c5934
Kian <78940146+WinchesterAge@users.noreply.github.com>
2022-07-22 19:49:22 +0430

    for merge

--- eshop_account/tests.py
+++ eshop_account/tests.py
@@ -3,7 +3,15 @@ from django.test import TestCase
 
 class Account_TestCase(TestCase):
     def setUp(self):
-        User.objects.create(username="Test",password="Test",email="test@test.com")
+        u = User.objects.create(username="Test",password="Test",email="test@test.com")
+        u.reset_password.reset_code = 56544
     def test_user(self):
-        user = User.objects.filter(username="Test").first()
+        user = User.objects.filter(username="Test",reset_password__is_baned=False).first()
         self.assertEqual(user.email,"test@test.com")
+        self.assertEqual(user.reset_password.is_baned,False)
+        self.assertFalse(user.reset_password is None)
+
+class home_page_test(TestCase):
+    def test_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code ,200)
